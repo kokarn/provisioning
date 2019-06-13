8111018db08b1350f1260c5cb71a3f6495b354e8
Oskar Risberg <oskar.risberg@stendahls.se>
2019-06-13 17:41:18 +0200

    Use a proper npm package

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var plist = require('simple-plist');
 
 module.exports = function(file, callback){
-  var certDl = new (require('cert-downloader'));
+  var certDl = new (require('@stendahls/cert-downloader'));
   certDl.verify(file, function(error, output) {
     if(error){
       return callback(error);
